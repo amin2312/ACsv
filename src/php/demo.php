cc318191c6a3522451f593d4040ef9444afb19a6
amin2312 <aminlab@qq.com>
2022-04-28 21:57:40 +0800

    v1.0

--- src/php/demo.php
+++ src/php/demo.php
@@ -2,36 +2,6 @@
 require "ACsv.php";
 use acsv\Table as Table;
 /**
- * Standard csv format text
- */
-$standard_format_text = 'id,id2,id3,name,brief
-1,20,100,John,He is a googd man
-2,20,100,张三,"他是一个好人
-我们都喜欢他"
-3,21,100,море,"Он хороший человек
-мы все любим его
-Его девиз:
-""доверяй себе"""
-4,21,200,الشمس,صباح الخير
-5,22,200,चंद्रमा,सुसंध्या
-6,22,200,ดาว,';
-$enhanced_format_text = 'id:int,id2:int,id3:int,name:string,weight:number,marry:bool,education:json,tags:strings,brief
-1,21,100,John,120.1,true,"[""AB""]","good,cool","Today is good day
-Tomorrow is good day too"
-2,21,100,张三,121.2,false,"[""CD"",""EF""]",good,今天是个好日子
-3,22,100,море,123.4,true,"[""GH"",""AB"",""CD""]",good,"Сегодня хороший день
-Завтра тоже хороший день"
-4,22,200,الشمس,124.5,false,"{""AA"":12}",strong,صباح الخير
-5,23,200,चंद्रमा,126.7,1,"{""BB"":12}",strong,सुसंध्या
-6,23,200,Emilia,,0,"{""CC"":67,""DD"":56}","strong,cool",Hoje é um bom dia
-7,24,300,Ayşe,128.9,0,"{""EE"":68,""FF"":56}","strong,cool",Bugün güzel bir gün
-8,24,300,陽菜乃,129.01,,"{""AC"":78,""BD"":[90,12]}","height,strong",今日はいい日です
-9,25,300,Dwi,130.12,1,"{""EF"":78,""CF"":[90,12]}",,"Hari ini adalah hari yang baik
-Besok juga hari yang baik"
-10,25,400,Bảo,131.23,1,"[""BC"",{""AT"":34}]","thin,good",
-11,26,400,민준,132.34,0,"[""FG"",{""AG"":34}]","hot,thin,good",오늘은 좋은 날이다
-12,26,400,ดาว,133.456,0,,,';
-/**
  * Tables
  */
 $_tab1;
@@ -41,10 +11,12 @@ $_tab2;
  */
 function main()
 {
-    global $_tab1, $_tab2, $standard_format_text, $enhanced_format_text;
+    global $_tab1, $_tab2;
+    $standard_format_text = file_get_contents(__DIR__ .'/../../release/csvs/standard_format_text.csv');
     $_tab1 = Table::Parse($standard_format_text);
-    $_tab2 = Table::Parse($enhanced_format_text);
     test_standard_csv_format();
+    $enhanced_format_text = file_get_contents(__DIR__ .'/../../release/csvs/enhanced_format_text.csv');
+    $_tab2 = Table::Parse($enhanced_format_text);
     test_enhanced_csv_format();
 }
 function P($cmd, $o)
